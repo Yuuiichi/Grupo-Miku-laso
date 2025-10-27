114bb4c903cf9d1cbe78ad110e3f032ab4ce8c23
Daspssj <diegosierraaaaa@gmail.com>
2025-10-27 05:27:13 -0300

    Listo con el dia 2

---
+++ app/schemas/prestamo.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+from typing import List, Optional
+from datetime import datetime, time
+
+class PrestamoCreate(BaseModel):
+    tipo_prestamo: str
+    usuario_id: int
+    bibliotecario_id: int
+    ejemplares_ids: List[int]
+
+class DetallePrestamoResponse(BaseModel):
+    ejemplar_id: int
+
+class PrestamoResponse(BaseModel):
+    id: int 
+    tipo_prestamo: str
+    usuario_id: int
+    bibliotecario_id: int
+    fecha_prestamo: datetime
+    fecha_devolucion_estimada: Optional[datetime]
+    estado: str
+    detalles: List[DetallePrestamoResponse] = []
+
+    class Config:
+        orm_mode = True
