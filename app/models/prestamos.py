00544d6fa8335d0e6f3cd5f91ae159ac50674110
Daspssj <diegosierraaaaa@gmail.com>
2025-10-27 04:47:34 -0300

    Listo con las cosas del dia 1

---
+++ app/models/prestamos.py
@@ -0,0 +1,39 @@
+from sqlalchemy import Column, Integer, String, ForeingKey, DateTime, Time, Enum
+from sqlalchemy.orm import relationship
+from database import Base
+import enum
+
+class TipoPrestamo(enum.Enum):
+    sala = "sala"
+    domicilio = "domicilio"
+
+class EstadoPrestamo(enum.Enum):
+    activo = "activo"
+    devuelto = "devuelto"
+    vencido = "vencido"
+
+class Prestamo(Base): 
+    __tblename__ = "prestamos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    tipo_prestamo = Column(Enum(TipoPrestamo), nullable=False)
+    usuario_id = Column(Integer, ForeignKey("usuarios.id"), nullable=False)
+    biblioteca_id = Column(Integer, ForeignKey("bibliotecas.id"), nullable=False)
+    fecha_prestamo = Column(DateTime, nullable=False)
+    hora_prestamo = Column(Time, nullable=False)
+    fecha_devolucion_estimada = Column(DateTime, nullable=True)
+    hora_devolucion_estimada = Column(Time, nullable=True)
+    fecha_devolucion_real = Column(DateTime, nullable=True)
+    hora_devolucion_real = Column(Time, nullable=True)
+    estado = Column(Enum(EstadoPrestamo), default=EstadoPrestamo.activo)
+
+    detalles = relationship("DetallePrestamo", back_populates="prestamo")
+
+class DetallePrestamo(Base):
+    __tablename__ = "detalles_prestamo"
+
+    id = Column(Integer, primary_key=True, index=True)
+    prestamo_id = Column(Integer, ForeignKey("prestamos.id"), nullable=False)
+    ejemplar_id = Column(Integer, ForeignKey("ejemplares.id"), nullable=False)
+
+    prestamo = relationship("Prestamo", back_populates="detalles")
